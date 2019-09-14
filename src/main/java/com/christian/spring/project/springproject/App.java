d61a55c934ffc0240a6cf66681053adbf989dbae
Christian <ctabarangao@qaconsultants.com>
2019-09-14 08:12:38 -0700

    Modified gitignore and added beans.xml along with its' creation of package for organization purposes

--- src/main/java/com/christian/spring/project/springproject/App.java
+++ src/main/java/com/christian/spring/project/springproject/App.java
@@ -1,12 +1,19 @@
 package com.christian.spring.project.springproject;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * Hello world!
  *
  */
 public class App {
 	public static void main(String[] args) {
-		Person person = new Person();
+		ApplicationContext applicationContext = new ClassPathXmlApplicationContext(
+				"com/christian/spring/project/bean/beans.xml");
+
+		Person person = (Person) applicationContext.getBean("person");
 		person.speak();
+		((ClassPathXmlApplicationContext) applicationContext).close();
 	}
 }
